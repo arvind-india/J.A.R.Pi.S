eb71b28f66fd7285b47bc895fbf200f87b42985a
Kevin Bastian <kbastian@meyle-mueller.de>
2017-02-15 15:11:30 +0100

    refactoring and fixing

--- jarpis/event.py
+++ jarpis/event.py
@@ -1,5 +1,6 @@
-from jarpis import conn
 import sqlite3
+
+conn = None
 
 class Event(object):
     def __init__(self, id, description, start, end, private, creator, type, series):
@@ -14,7 +15,10 @@ class Event(object):
 
     def create(self):
         c = conn.cursor()
-        c.execute("INSERT INTO EVENT (ID, DESCRIPTION, START_DATE, END_DATE, PRIVATE, FK_CREATOR, FK_TYPE, FK_SERIES) VALUES(?,?,?,?,?,?,?,?)", (self._id,self._description,self._start,self._end,self._private,self._creator,self._type,self._series))
+        c.execute(
+            "INSERT INTO EVENT (ID, DESCRIPTION, START_DATE, END_DATE, PRIVATE, FK_CREATOR, FK_TYPE, FK_SERIES) VALUES(?,?,?,?,?,?,?,?)",
+            (self._id, self._description, self._start, self._end, self._private, self._creator, self._type,
+             self._series))
         conn.commit()
         return self
 
@@ -35,23 +39,26 @@ class Event(object):
         b = c.fetchone()
 
         if b != None:
-            return Event(b[0],b[1],b[2],b[3],b[4],b[5],b[6],b[7])
+            return Event(b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7])
 
         raise EventNotFoundException("No Event found with given ID: %s" % (id))
 
     def __repr__(self, *args, **kwargs):
-        return "ID=%s, Description=%s, START=%s, END=%s, PRIVATE=%s, CREATOR=%s, TYPE=%s, SERIES=%s, SUPER=%s" % (self._id, self._description, self._start, self._end, self._private, self._creator, self._type, self._series, super().__repr__())
+        return "ID=%s, Description=%s, START=%s, END=%s, PRIVATE=%s, CREATOR=%s, TYPE=%s, SERIES=%s, SUPER=%s" % (
+        self._id, self._description, self._start, self._end, self._private, self._creator, self._type, self._series,
+        super().__repr__())
+
 
 class EventNotFoundException(Exception):
     pass
 
 
 class DBUtil():
-
     result = None
 
     @staticmethod
     def exec(callback, *args):
+        global conn
         conn = sqlite3.connect("develop.db")
         result = callback(*args)
         conn.close()
