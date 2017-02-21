23ae4703425f5a276de5d1376f7a275712717aec
Kevin Bastian <kbastian@meyle-mueller.de>
2017-02-21 16:38:53 +0100

    removed usless command

--- tests/test_privacy.py
+++ tests/test_privacy.py
@@ -6,7 +6,6 @@ class PrivacyTest(unittest.TestCase):
 
     def setUp(self):
         TestDBUtil.exec(Privacy.createPrivacyTable, [])
-        TestDBUtil.exec()
 
     def testWhatEver(self):
         self.assertTrue(True)
