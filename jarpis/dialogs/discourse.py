91024a3626d0e20ac0ac5e883a3624801c091585
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-03 21:11:15 +0100

    Implement node of the discourse tree
    First approach to implement the discourse unit evaluation
    which represents a node in the discourse tree.
    But the evaluation process needs more thought.

--- jarpis/dialogs/discourse.py
+++ jarpis/dialogs/discourse.py
@@ -3,13 +3,49 @@ import jarpis.dialogs
 
 class DialogManager:
 
-    def __init__():
+    def __init__(self):
         pass
 
 
 class DiscourseUnit:
-    pass
+
+    def __init__(self, evaluation_strategy, semantic_object_type, children):
+        self._evaluation_strategy = evaluation_strategy
+        self._is_evaluated = False
+        self._type = semantic_object_type
+        self._children = children
+        self._semantic_object = None
+
+    def evaluate(self):
+        '''
+        TODO when is a discourse unit considered to be evaluated?
+        the tree should collapse upwards and children should be removed from
+        their parent. If the root node has no children it is considered to be
+        evaluated and can be semantically bound to the domain object
+        '''
+        for child_unit in self._children:
+            if not child_unit._is_evaluated:
+                child_unit.evaluate()
+
+        if self.semantic_object is not None:
+            jarpis.dialogs.communication.publish(
+                "requestEvaluation", self.semantic_object)
+        else:
+            raise SemanticEvaluationError(
+                "Cannot evaluate semantic object that is None.")
+
+    @property
+    def semantic_object(self):
+        return self._semantic_object
+
+    @semantic_object.setter
+    def semantic_object(self, value):
+        self._semantic_object = value
 
 
 class DiscourseTree:
+    pass
+
+
+class SemanticEvaluationError(Exception):
     pass
