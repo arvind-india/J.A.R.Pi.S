ddf202e9d4ea488f3c21e8dd559e249abde5d894
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-04 16:24:51 +0100

    Add callbacks for DialogManager and registered them
    Added the callbacks for the Dialog Manager that handle the published events
    from the semantic interpreter and the discourse analysis modules.

--- jarpis/dialogs/discourse.py
+++ jarpis/dialogs/discourse.py
@@ -4,6 +4,41 @@ import jarpis.dialogs
 class DialogManager:
 
     def __init__(self):
+        self._register_events()
+
+    def _register_events(self):
+        # semantic interpreter events
+        jarpis.dialogs.communication.register(
+            "interpretationSuccessfull", self._insert_in_discourse_tree)
+        jarpis.dialogs.communication.register(
+            "nothingToInterpret", self._nothing_to_interpret)
+        jarpis.dialogs.communication.register(
+            "interpretationFinished", self._start_semantic_evaluation)
+
+        # discourse analysis events
+        jarpis.dialogs.communication.register(
+            "evaluationSuccessfull", self._semantic_object_evaluated)
+        jarpis.dialogs.communication.register(
+            "evaluationFailed", self._semantic_object_evaluation_failed)
+        jarpis.dialogs.communication.register(
+            "evaluationFailed", self._invalid_semantic_object_information)
+
+    def _insert_in_discourse_tree(self, semantic_object):
+        pass
+
+    def _start_semantic_evaluation(self):
+        pass
+
+    def _nothin_to_interpret(self):
+        pass
+
+    def _semantic_object_evaluated(self, semantic_object):
+        pass
+
+    def _semantic_object_evaluation_failed(self, semantic_object):
+        pass
+
+    def _invalid_semantic_object_information(self, semantic_object):
         pass
 
 
