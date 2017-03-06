add89d9703d8af081719e7cc610494f98ccffaba
Kevin Bastian <kebastian92@gmail.com>
2017-03-06 20:18:28 +0100

    refactoring

--- jarpis/event.py
+++ jarpis/event.py
@@ -126,8 +126,8 @@ class Birthday(Event):
     def fromResultToObject(result):
         identity = result[0]
         params = EventParameter.loadParameterById(identity)
-        ev = Birthday(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], params)
-        return ev
+        event = Birthday(result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7], params)
+        return event
 
     def create(self):
         super(Birthday, self).create()
