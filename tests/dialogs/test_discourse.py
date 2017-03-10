3d8d22db8157355a40a02bbcfe0d85d5e2238212
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-10 16:25:38 +0100

    Add defined semantic object insertion test methods

---
+++ tests/dialogs/test_discourse.py
@@ -0,0 +1,16 @@
+from unittest import TestCase
+
+
+class A_semantic_object_can_be_correctly_inserted_into_a_discourse_tree(TestCase):
+
+    def test_if_at_least_one_tree_has_a_discourse_unit_with_the_same_entity_type_as_the_semantic_object(self):
+        self.fail()
+
+    def test_if_the_appropriate_discourse_unit_is_the_tree_root(self):
+        self.fail()
+
+    def test_if_the_appropriate_discourse_unit_is_an_inner_node(self):
+        self.fail()
+
+    def test_if_the_appropriate_discourse_unit_is_a_leaf(self):
+        self.fail()
