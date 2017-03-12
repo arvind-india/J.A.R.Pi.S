ee75873b26d245c733b6ad8e1f5ae49e617d8b40
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-12 18:19:35 +0100

    Add event related semantic frame definitions

--- jarpis/dialogs/frames.py
+++ jarpis/dialogs/frames.py
@@ -1,12 +1,99 @@
+EventName = {}
+EventType = {}
+Date = {}
+
+Period = {
+    "entityType": None,
+    "slots": {
+        "start": Date,
+        "end": Date
+    }
+}
+
+Username = {}
+
+User = {
+    "entityType": "User",
+    "slots": {
+        "name": Username
+    }
+}
+
+Scheduling = {
+    # TODO define
+}
+
+Privacy = {}
+
+Visibility = {
+    "entityType": "Visibility",
+    "slots": {
+        "privacy": Privacy
+    }
+}
+
+Item = {
+    # TODO define
+}
+
 Event = {
     "entityType": "Event",
     "slots": {
         "name": EventName,
         "type": EventType,
-        "timespan": Timespan,
-        "creator": User,
         "period": Period,
+        "creator": User,
+        "scheduling": Scheduling,
         "visibility": Visibility,
         "listItems": [Item]
+    }
+}
+
+EventByName = {
+    "entityType": "Event",
+    "slots": {
+        "name": EventName
+    }
+}
+
+EventByDate = {
+    "entityType": "Event",
+    "slots": {
+
+    }
+}
+
+EventsByTypeQuery = {
+    "entityType": None,
+    "slots": {
+        "type": EventType,
+        "events": [Event]
+    }
+}
+
+EventsByDateQuery = {
+    "entityType": None,
+    "slots": {
+        "period": Period,
+        "events": [Event]
+    }
+}
+
+EventsByCreatorQuery = {
+    "entityType": None,
+    "slots": {
+        "creator": User,
+        "period": Period,
+        "events": [Event]
+    }
+}
+
+EventsByVisibilityQuery = {
+    "entityType": None,
+    "slots": {
+        "visibility": Visibility,
+        "creator": User,
+        "period": Period,
+        "events": [Event]
     }
 }
