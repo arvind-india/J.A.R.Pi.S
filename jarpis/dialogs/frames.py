030adb7d3a7b9f351609d18265f22bdd149d177f
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-12 17:47:21 +0100

    Add semantic frame definition as dictionary

---
+++ jarpis/dialogs/frames.py
@@ -0,0 +1,12 @@
+Event = {
+    "entityType": "Event",
+    "slots": {
+        "name": EventName,
+        "type": EventType,
+        "timespan": Timespan,
+        "creator": User,
+        "period": Period,
+        "visibility": Visibility,
+        "listItems": [Item]
+    }
+}
