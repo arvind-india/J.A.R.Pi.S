d2637616b72e1e6805c855a9f3be9b3bf80b4978
RuWe <ruthweber95@web.de>
2017-03-26 15:54:55 +0200

    Added that when Speaker is not identified it returns a anonymus user

--- jarpis/recognition/speakerRecognition.py
+++ jarpis/recognition/speakerRecognition.py
@@ -16,6 +16,7 @@ def get_current_speaker():
             if speaker[1] == name:
                 return speaker
             else:
-                return None
+                return ("anonymus", None)
     except IOError as err:
         print("IOError: {0}").format(err)
+
