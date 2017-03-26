b61a4713ac652ad9a8d0a0b9ab986c4026bcb034
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-26 16:54:12 +0200

    Implement user binding after merge

--- jarpis/dialogs/discourse.py
+++ jarpis/dialogs/discourse.py
@@ -1,6 +1,7 @@
 import jarpis.dialogs
 from jarpis.user import User, UserNotFoundException
 from jarpis.dialogs.semantics import SemanticClass
+import jarpis.recognition.speakerRecognition as speakerRecognition
 
 
 class DiscourseAnalysis:
@@ -18,21 +19,25 @@ class DiscourseAnalysis:
             self._bind_user(semantic_object)
 
     def _bind_user(self, semantic_object):
+        communication = jarpis.dialogs.communication
         semantic_class = semantic_object.semantic_class
         if semantic_class == "UserByReference":
             # this means the user said something with a self reference like:
             # "for me, my, I"
-            # -> get the user from the speaker recognition
-            speaker = None
+            speaker = speakerRecognition.get_current_speaker()
+
+            if speaker[0] == "anonymous":
+                communication.publish("evaluationFailed", semantic_object)
+                return
+
             try:
                 user = User.getUserFromSpeaker(speaker)
             except UserNotFoundException:
-                # no user for speaker! publish error
+                communication.publish("invalidInformation", semantic_object)
                 return
 
             # TODO either fill a dict or have SemanticClass subclass for each
             # entity
             bound_object = semantic_object.bind({})
             bound_object.entity = user
-            jarpis.dialogs.communication.publish(
-                "evaluationSuccessful", bound_object)
+            communication.publish("evaluationSuccessful", bound_object)
