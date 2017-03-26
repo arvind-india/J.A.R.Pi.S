bfa42554ca5c773e821776d9100b8fa138bb724a
dschaufelberger <dominik.schaufelberger@web.de>
2017-03-26 13:26:04 +0200

    Add class definition and registered events

--- jarpis/dialogs/discourse.py
+++ jarpis/dialogs/discourse.py
@@ -1,1 +1,14 @@
 import jarpis.dialogs
+
+
+class DiscourseAnalysis:
+
+    def __init__(self):
+        self._register_events()
+
+    def _register_events(self):
+        jarpis.dialogs.communication.register(
+            "evaluationRequest", self._evalute)
+
+    def _evaluate(self, semantic_object):
+        pass
